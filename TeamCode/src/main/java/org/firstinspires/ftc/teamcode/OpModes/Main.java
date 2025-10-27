4230a14c34e6b7d512e8180e2364045c279cb655
Andrew Bota <andrewcbota@icloud.com>
2025-10-27 16:59:09 -0700

    Working on TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main.java
@@ -2,20 +2,30 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
-import org.firstinspires.ftc.ftccommon.internal.manualcontrol.responses.HubType;
 import org.firstinspires.ftc.teamcode.Subsystems.SubsystemCollection;
 
 import java.util.List;
 
 public class Main extends OpMode {
     SubsystemCollection sys;
+    GamepadEx Gamepad1;
+    GamepadEx Gamepad2;
 
     @Override
     public void init() {
+        // A class that contains all of the subsystems.
+        // It is used as a singleton to make sure that every OpMode
+        // and (future) Command references the same subsystem instance.
         SubsystemCollection.deinit();
         sys = SubsystemCollection.getInstance(hardwareMap);
 
+        Gamepad1 = new GamepadEx(gamepad1);
+        Gamepad2 = new GamepadEx(gamepad2);
+
+        // Enable bulk cached reading to speed up I2C read times.
         List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : allHubs) {
             hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
@@ -26,7 +36,7 @@ public class Main extends OpMode {
     public void start() {
         // As soon as the match starts (not when we init, when the match actually starts
         // and we're allowed to move), we want the shooter to get up to speed.
-        sys.shooter.setPower(1.0);
+//        sys.shooter.setPower(1.0);
     }
 
     @Override
@@ -39,30 +49,35 @@ public class Main extends OpMode {
 
         // Gather joystick values from LX, LY (inverted, so up is forward y),
         // and RX (for turning) on gamepad1.
-        double cX = gamepad1.left_stick_x;
-        double cY = -gamepad1.left_stick_y;
-        double cA = gamepad1.right_stick_x;
+        double cX = Gamepad1.getLeftX();
+        double cY = -Gamepad1.getLeftY();
+        double cA = Gamepad1.getRightX();
 
         // Use the trigger for manual speed control, added on top of the joystick magnitude.
         // This is set to the gamepad1 right trigger.
-        double speedMultiplier = gamepad1.right_trigger;
+        double speedMultiplier = Gamepad1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
 
         // Robot-centric drive with the joystick inputs from earlier lines.
         // I multiplied by speedMultiplier to allow using the trigger.
         sys.drivebase.Drive(cX * speedMultiplier, cY * speedMultiplier, cA * speedMultiplier, -1);
 
-        if (gamepad1.dpad_up) sys.drivebase.DriveFieldCentric(0, 1, 0, -1);
-        if (gamepad1.dpad_down) sys.drivebase.DriveFieldCentric(0, -1, 0, -1);
-        if (gamepad1.dpad_left) sys.drivebase.DriveFieldCentric(-1, 0, 0, -1);
-        if (gamepad1.dpad_right) sys.drivebase.DriveFieldCentric(1, 0, 0, -1);
+        // Field-centric drive using the D-Pad inputs. Up goes up (y+), left goes left (x-), etc.
+        // Maybe should have braces for each case. However, you're never going to press more than
+        // one D-Pad input at a time.
+        if (Gamepad1.wasJustPressed(GamepadKeys.Button.DPAD_UP)) sys.drivebase.DriveFieldCentric(0, 1, 0, -1);
+        if (Gamepad1.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) sys.drivebase.DriveFieldCentric(0, -1, 0, -1);
+        if (Gamepad1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) sys.drivebase.DriveFieldCentric(-1, 0, 0, -1);
+        if (Gamepad1.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) sys.drivebase.DriveFieldCentric(1, 0, 0, -1);
 
         //endregion
 
         //region Utility Functions (with gamepad2)
 
-        if (gamepad2.left_bumper) sys.hopper.setCounter(sys.hopper.getCounter() - 1);
-        else if (gamepad2.right_bumper) sys.hopper.setCounter(sys.hopper.getCounter() + 1);
-
+        if (Gamepad2.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+            sys.hopper.setCounter(sys.hopper.getCounter() - 1);
+        } else if (gamepad2.right_bumper) {
+            sys.hopper.setCounter(sys.hopper.getCounter() + 1);
+        }
 
         //endregion
     }
